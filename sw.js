dc76df729bdf109b18f0a2755e8866fb656bb7e7
incomeengine12 <314488812+incomeengine12@users.noreply.github.com>
2026-08-16 01:43:41 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v369';
-const APP_BUILD = 369; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v370';
+const APP_BUILD = 370; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [
